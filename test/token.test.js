6986a8db56771decb3f2fc06b1a6aa94a83ee19d
NotDeffect <deyan@defoundation.xyz>
2022-06-10 16:36:12 +0300

    hardhat reset fork config

--- test/token.test.js
+++ test/token.test.js
@@ -3,11 +3,31 @@ const { ethers } = require("hardhat");
 
 
 describe("NFT royalty token test", function () {
+    let royaltzNFT;
+
     before(async () => {
         const [deployer, treasury] = await ethers.getSigners()
         const RoyaltzNFT = await ethers.getContractFactory("RoyaltzNFT")
-        const royaltzNFT = await RoyaltzNFT.deploy(deployer.address);
+        royaltzNFT = await RoyaltzNFT.deploy(deployer.address);
+
+        const tokens = await royaltzNFT.tokensOfOwner(deployer.address);
+        console.log('tokens: ', tokens);
     })
+
+    beforeEach(async () => {
+
+        await ethers.provider.send(
+            "hardhat_reset",
+            [
+                {
+                    forking: {
+                        jsonRpcUrl: "https://api.avax-test.network/ext/bc/C/rpc"
+                    }
+                }
+            ]
+        );
+        // send any token amounts here
+    });
 
     it("Initial", () => {
         console.log('Hello Deffect...!');
